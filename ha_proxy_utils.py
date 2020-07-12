7d35e76721e91bc0e9f5261b3e43ff9b650da154
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2020-07-12 08:05:19 +0500

    updated ha proxy to use system config

--- ha_proxy_utils.py
+++ ha_proxy_utils.py
@@ -1,30 +1,40 @@
 from pyhaproxy.parse import Parser
 from pyhaproxy.render import Render
 import pyhaproxy.config as config
-import uuid
-CONFIG_PATH = './haproxy.cfg'
+import os
+
+CONFIG_PATH = '/etc/haproxy/haproxy.cfg'
+
+SERVICE_PORT = '5000'
 
 BACKEND_NAME = 'flask_backend'
 
+INVALIDATE_CONFIG_COMMAND = 'sudo systemctl restart haproxy'
 
 def create_server(name, host):
     server = config.Server(
-        name=name, host=host, port='5000', attributes=['check'])
+        name=name, host=host, port=SERVICE_PORT, attributes=['check'])
     return server
 
-
 def set_containers(containers):
+    ## Read the current configuration and parse it
     parser = Parser(CONFIG_PATH)
     configuration = parser.build_configuration()
     backend = configuration.backend(BACKEND_NAME)
 
+    ## remove all current servers from backend configuration
     configured_servers = backend.servers()
     for server in configured_servers:
         backend.remove_server(server.name)
 
+    ## add servers according to the running docker containers
     for container in containers:
         server = create_server(name=container['id'], host=container['ip'])
         backend.add_server(server)
+
+    ## update the ha proxy configuration
     config_render = Render(configuration)
     config_render.dumps_to(CONFIG_PATH)
 
+    ## Restart the service for changes to take effect
+    os.system(INVALIDATE_CONFIG_COMMAND)
